df41af8d737cde76b8431d7577376167651c4c2c
Shawn Martin <bh74sa@student.sunderland.ac.uk>
2022-01-10 23:24:55 +0000

    Added sign up functionality to Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,6 @@
-import React, { useRef } from "react";
-import { Button, Card, Form } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Button, Card, Form, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Signup() {
   //declaring refs, so we can access the DOM
@@ -7,13 +8,52 @@ export default function Signup() {
   const passwordRef = useRef();
   const passConfRef = useRef();
 
+  //taking signup() from 
+  const {signup} = useAuth();
+
+  //error state, if sign up unsuccessful
+  const [error, setError] = useState('');
+
+  //loading state, to prevent multiple requests to sign up when it's loading
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    //prevents page from loading
+    e.preventDefault();
+
+    let email = emailRef.current.value;
+    let password = passwordRef.current.value;
+    let passConf = passConfRef.current.value;
+
+    //authenticate password
+    if (password !== passConf) {
+      // "return" will force to exit out of the function execution
+      return setError('Passwords do not match');
+    }
+
+    try {
+      setError(''); //resets error back to '', so no previous errors are caught
+      setLoading(true); //loading..
+
+      await signup(email, password)
+    } catch  {
+      setError('Failed to create account. Please try again');
+    }
+
+    setLoading(false); //loading finished
+
+    
+  }
+
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
 
-          <Form>
+          <Form onSubmit={handleSubmit}>
+            {/* if (error = ''), (error &&) will result to false. If the condition is true, the element right after && will appear in the output. If it is false, React will ignore and skip it.,  */}
+            {error && <Alert variant="danger">{error}</Alert>}
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" required ref={emailRef} />
@@ -29,10 +69,12 @@ export default function Signup() {
               <Form.Control type="email" required ref={passConfRef} />
             </Form.Group>
             
-            <Button className="w-100" type="submit">
+            <Button className="w-100" type="submit" disabled={loading}>
               Sign Up
             </Button>
+
           </Form>
+
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
