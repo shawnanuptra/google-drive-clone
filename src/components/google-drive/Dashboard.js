373849d9bc28553191225f439e98e6b29df10de9
Shawn Martin <bh74sa@student.sunderland.ac.uk>
2022-01-18 19:47:42 +0000

    Allows adding child folders functionality

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -3,17 +3,33 @@ import { Container } from 'react-bootstrap'
 import { useFolder } from '../../hooks/useFolder'
 import AddFolderButton from './AddFolderButton'
 import Navbar from './Navbar'
+import Folder from './Folder'
 
 export default function Dashboard() {
 
-    const { folder } = useFolder('fJMdTTzVRmYqrlQS3IUs');
-    console.log(folder)
+    const { folder, childFolders } = useFolder('fJMdTTzVRmYqrlQS3IUs');
+    // console.log(childFolders[0].id)
+    console.log(childFolders.length)
     return (
         <>
-        <Navbar />
-        <Container fluid>
-            <AddFolderButton currentFolder={folder}/>
-        </Container>
+            <Navbar />
+            <Container fluid>
+                <AddFolderButton currentFolder={folder} />
+                {childFolders.length > 0 && (
+                    <div className='d-flex flex-wrap'>
+
+                        {childFolders.map(childFolder => {
+                            return (
+                                <div key={childFolder.id} style={{ maxWidth: '250px' }} className='p-2'>
+                                    <Folder folder={childFolder} />
+                                </div>
+                            )
+                        })}
+
+                    </div>
+
+                )}
+            </Container>
         </>
     )
 }
--- src/hooks/useFolder.js
+++ src/hooks/useFolder.js
@@ -1,19 +1,22 @@
-import { doc, DocumentReference, getDoc } from "firebase/firestore"
+import { doc, getDoc, getDocs, orderBy, query, where, onSnapshot } from "firebase/firestore"
 import { useEffect } from "react"
 import { useReducer } from "react"
+import { useAuth } from "../contexts/AuthContext"
 import { database, firestore } from "../firebase"
 
 
 //ACTIONS object - just to prevent typos
 const ACTIONS = {
     SELECT_FOLDER: 'select-folder',
-    UPDATE_FOLDER: 'update-folder'
+    UPDATE_FOLDER: 'update-folder',
+    SET_CHILD_FOLDERS: 'set-child-folders'
 }
 
 //Root folder object
 const ROOT_FOLDER = {
     name: 'Root', id: null, path: []
 }
+
 
 //reducer function for useReducer()
 function reducer(state, { type, payload }) {
@@ -32,20 +35,27 @@ function reducer(state, { type, payload }) {
                 ...state,
                 folder: payload.folder
             }
+        case ACTIONS.SET_CHILD_FOLDERS:
+            return {
+                ...state,
+                childFolders: payload.childFolders
+            }
         default:
             return state;
     }
 }
 
 export function useFolder(folderID = null, folder = null) {
-    
+
+    const { currentUser } = useAuth();
+
     //useReducer function
-    const [state, dispatch] = useReducer(reducer,{
-            folderID,
-            folder,
-            childFolders: [],
-            childFiles: []
-        })
+    const [state, dispatch] = useReducer(reducer, {
+        folderID,
+        folder,
+        childFolders: [],
+        childFiles: []
+    })
 
     //triggered when user selects a folder
     useEffect(() => {
@@ -53,7 +63,7 @@ export function useFolder(folderID = null, folder = null) {
     }, [folderID, folder])
 
     //triggered when only folderID is changed e.g. when change pages
-    useEffect( async () => {
+    useEffect(async () => {
         //'Root', where folderID is null
         if (folderID == null) {
             return dispatch({
@@ -63,11 +73,11 @@ export function useFolder(folderID = null, folder = null) {
         }
 
         //addFolder in current folder
-        const docRef = doc(firestore, 'folders', folderID )
+        const docRef = doc(firestore, 'folders', folderID)
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
-            
+
             dispatch({
                 type: ACTIONS.UPDATE_FOLDER,
                 payload: { folder: database.formatDoc(docSnap) }
@@ -82,5 +92,31 @@ export function useFolder(folderID = null, folder = null) {
 
     }, [folderID])
 
+    //triggered when childFolder is added
+    useEffect(async () => {
+
+        // where('parentID', "==", folderID).where("userID", "==", currentUser.uid)
+        // orderBy("createdAt")
+        // onSnapshot(snapshot, dispatch({
+        //     type: ACTIONS.SET_CHILD_FOLDERS,
+        //     payload: { childFolders: snapshot.docs.map(database.formatDoc) }
+
+        // }))
+
+
+        const q = query(database.folders, where('parentID', "==", folderID), where("userID", "==", currentUser.uid), orderBy('createdAt'));
+
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            dispatch({
+                type: ACTIONS.SET_CHILD_FOLDERS,
+                payload: {
+                    childFolders: querySnapshot.docs.map(database.formatDoc)
+                }
+            })
+        });
+
+        return unsubscribe;
+
+    }, [folderID, currentUser])
     return state;
 }
